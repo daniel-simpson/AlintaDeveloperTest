9ef39975816b1e809b6420a92e5fde1d26a1a087
Daniel Simpson <daniel_simpson1988@hotmail.com>
2019-08-18 17:32:35 +1000

    Mock out CustomerService and interface

---
+++ src/Alinta.Customers/Services/CustomerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Alinta.Customers.Models;
+
+namespace Alinta.Customers.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        public Task AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> Search(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
